04fdce363ef6b78b2b9b67807a72525cbe622773
kiboma <omayioben@gmail.com>
2022-11-23 16:35:16 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :greetings
+    end
+  end
+  
   resources :greetings
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
